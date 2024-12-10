a77b858d0a716956658d8f356e9530b60097a73f
S0ME0NE03 <128107137+S0ME0NE03@users.noreply.github.com>
2024-12-10 08:48:29 -0800

    Create math_launcher.py

---
+++ math_launcher.py
@@ -0,0 +1,7 @@
+while 1:
+  ip = input("what do you want to run(prime factor, childern in basement calclulator)')
+  if ip == "prime factor":
+      exec(open("prime_factor.py").read())
+
+  elif ip == "childern in basement calclulator":
+    exec(open("prime_factor.py").read())
