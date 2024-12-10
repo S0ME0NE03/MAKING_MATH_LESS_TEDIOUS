0bb127e4c022a23a5dc4b9ca21bf432fcb71ac2d
S0ME0NE03 <128107137+S0ME0NE03@users.noreply.github.com>
2024-12-10 09:14:27 -0800

    Update math_launcher.py

--- math_launcher.py
+++ math_launcher.py
@@ -1,7 +1,15 @@
 while 1:
   ip = input("what do you want to run(prime factor, childern in basement calclulator)')
   if ip == "prime factor":
-      exec(open("prime_factor.py").read())
-
+      try:
+        exec(open("prime_factor.py").read())
+      except:
+        print("prime_factor.py is not downloaded or not in the curent folder, please download the progam at: \nhttps://github.com/S0ME0NE03/MAKING_MATH_LESS_TEDIOUS/blob/main/prime_factor.py\n')
   elif ip == "childern in basement calclulator":
-    exec(open("children_in_basement_calc.py").read())
+    try:
+      exec(open("children_in_basement_calc.py").read())
+    except:
+      print("children_in_basement_calc.py is not downloaded or not in the curent folder, please download the progam at: \nhttps://github.com/S0ME0NE03/MAKING_MATH_LESS_TEDIOUS/blob/main/children_in_basement_calc.py\n')
+  else:
+    print('not a avalible program')
+  
