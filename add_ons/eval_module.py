b025de8cd7fad02aa8a63c31268101800ef1101f
Op-25 <168381908+Op-25@users.noreply.github.com>
2024-12-10 21:52:51 -0800

    Add files via upload

---
+++ add_ons/eval_module.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter \"exit\" to exit")
+    while True:
+        action = input(": ")
+
+        if action == "quit" or action == "exit":
+            break
+            
+        else:
+            try:
+                print(eval(action))
+            except Exception as error:
+                print(f"Error: {error}")
---
+++ add_ons/get_time_module.py
@@ -0,0 +1,6 @@
+import time
+
+def main():
+    while True:
+        action = input("Enter an action: ")
+        #Future planes --- show current time, and also show time around the world, whereever you specify
