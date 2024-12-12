7fc2e8127eab9b8102f5107aef05ceffb79db650
S0ME0NE03 <128107137+S0ME0NE03@users.noreply.github.com>
2024-12-12 12:22:56 -0800

    Create downloader.py

---
+++ scripts/downloader.py
@@ -0,0 +1,3 @@
+import urllib.request
+def download(url, filename):
+  urllib.request.urlretrieve("https://raw.githubusercontent.com/S0ME0NE03/MAKING_MATH_LESS_TEDIOUS/refs/heads/Refactored_Launcher/add_ons/eval_module.py","IT_WORKDED.py")
