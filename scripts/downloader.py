585e67123b9dcbed4725d8afbd6315f4e7e4bc9f
S0ME0NE03 <128107137+S0ME0NE03@users.noreply.github.com>
2024-12-13 09:24:20 -0800

    Update downloader.py

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -1,7 +1,7 @@
 import urllib.request
 def download(url, filename):
-  urllib.request.urlretrieve("url","filename")
-if __name__ == __main__:
+  urllib.request.urlretrieve(url, filename)
+if __name__ == "__main__":
   url = input("Input a url:\n")
   filename = input("What should the file be named?\n")
   download(url, filename)
