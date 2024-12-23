3e1fca5eedb481ca71c0f06c80846f6315d70c84
Op-25 <168381908+Op-25@users.noreply.github.com>
2024-12-22 16:36:15 -0800

    Delete scripts/downloader.py

--- scripts/downloader.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-def download(url, filename):
-  urllib.request.urlretrieve(url, filename)
-def useless_func(x):
-  return x
-if __name__ == "__main__":
-  url = input("Input a url:\n")
-  filename = input("What should the file be named?\n")
-  download(url, filename)
