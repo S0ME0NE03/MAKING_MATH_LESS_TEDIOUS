b6089c07e19f65e922b15db43916d2d053927b4f
Op-25 <168381908+Op-25@users.noreply.github.com>
2024-12-24 23:33:06 -0800

    Delete add_ons/get_time_module.py

--- add_ons/get_time_module.py
+++
@@ -1,6 +0,0 @@
-import time
-
-def main():
-    while True:
-        action = input("Enter an action: ")
-        #Future planes --- show current time, and also show time around the world, whereever you specify
