9351a3cf29a86b812ba0f5c482a08258ac0ee2f0
S0ME0NE03 <128107137+S0ME0NE03@users.noreply.github.com>
2025-01-13 08:41:01 -0800

    Update main.py

--- add_ons/prime_factor/main.py
+++ add_ons/prime_factor/main.py
@@ -4,7 +4,24 @@ import urllib.request
 import pathlib
 
 path = pathlib.Path(__file__).parent.resolve()
+## load setings ##
+try:
+  config = open(path + "config.txt", "r")
+  config.readline
+  MAX_TIME = int(config.readline)
+  config.close
+except FileNotFoundError:
+  print("file not found")
+except SyntaxError:
+  print("there was a syntax error in the settings loading process, please report this to SOMEONE03 and i will fix this promptly")
+except Exception as e:
+  print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
+if MAX_TIME == None:
+  print("setings not defined loading default settings")
+  MAX_TIME = 5
 
+
+## end load setings ##
 def download(url, filename):
   urllib.request.urlretrieve(url, filename)
 def prime_scive(to_go_to: int) -> list[int]:
